a60d23c5c3379ce0158faf3357b216296efd24f2
Uziel <uac_h12@hotmail.com>
2014-09-30 14:25:44 +0000

    user create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,6 +29,12 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     inside_path
   end
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
+  def after_sign_up_path_for(resource)
+    new_user_session_path
+  end
   
   # Auto-sign out locked users
   def reject_locked!
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Facturas::Application.routes.draw do
   root "pages#home"    
   get "home", to: "pages#home", as: "home"
   get "inside", to: "pages#inside", as: "inside"
-  
     
   devise_for :users
   
